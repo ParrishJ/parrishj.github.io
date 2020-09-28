c51e1e8baec825adeda4f040bd6e02924345b372
jarparrish <jarparrish@gmail.com>
2020-09-27 18:13:11 -0700

    divider in place

--- js/unsplash.js
+++ js/unsplash.js
@@ -1,5 +1,5 @@
 function imageGetter () {
-  fetch('https://source.unsplash.com/random/400x400')
+  fetch('https://source.unsplash.com/random/500x500')
     .then(response => response)
     .then(data => {console.log(data);
       document.getElementById('random-image').setAttribute('src', data.url)
